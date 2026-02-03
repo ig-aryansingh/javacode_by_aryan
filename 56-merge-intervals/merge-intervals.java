c0424b6233f5065eae470b02db38066012614872
ig-aryansingh <aryan.singh.sde@gmail.com>
2026-02-03 17:46:02 +0530

    Time: 8 ms (90.00%) | Memory: 49.4 MB (11.01%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        if (intervals.length <= 1) return intervals;
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        List<int[]> merged = new ArrayList<>();
+        merged.add(intervals[0]);
+        for (int i = 1; i < intervals.length; i++) {
+            int[] last = merged.get(merged.size() - 1);
+            int[] curr = intervals[i];
+            if (curr[0] <= last[1]) { // Overlapping
+                last[1] = Math.max(last[1], curr[1]);
+            } else {
+                merged.add(curr);
+            }
+        }
+        return merged.toArray(new int[merged.size()][]);
+    }
+}
